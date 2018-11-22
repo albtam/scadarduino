57c6f77c9d097e8b0b0866c23244852f55326fd3
Alban <alban.tamisier@centrale-marseille.fr>
2018-11-23 00:09:59 +0100

    Add temperature and clock

--- project/project.ino
+++ project/project.ino
@@ -1,5 +1,10 @@
 #include <SD.h>
 #include <SPI.h>
+#include <OneWire.h> //Version 2.3.4
+
+// Date and time functions using a DS1307 RTC connected via I2C and Wire lib
+#include <Wire.h>
+#include "RTClib.h"
 
 // Configuration
 #define LOOP_TIME_MS        2000
@@ -8,12 +13,15 @@
 #define SINGLE_BATTERY_PIN A3
 
 // Digital Pins 
-const byte CSPIN          = 53; // SD card shield
+//const byte CSPIN                  = 57; // Micro SD card shield
+const byte CSPIN                  = 10; // VMA 202 shield
+const byte TEMPERATURE_SENSOR_PIN = 7;  // Temperature Sensor
 
 // Variable declarations
-String        columnNames         = "Timestamp; Single Battery Voltage";
-String        dataString          ;// holds the data to be written to the SD card
-unsigned long time;
+String     columnNames = "Date; Single Battery Voltage; Temperature";
+String     dataString; // holds the data to be written to the SD card
+RTC_DS1307 rtc;
+OneWire    ds(TEMPERATURE_SENSOR_PIN);
 
 // Local functions
 void writeOnSDcard (String dataString)
@@ -23,13 +31,64 @@ void writeOnSDcard (String dataString)
   {
     dataFile.println(dataString);
     dataFile.close();
-    Serial.println(dataString);// only for debugging
   }
  
   else // if the file isn't open, pop up an error
   {
     Serial.println("error opening datalog.csv");
   }
+}
+
+float getTemperatureInCelsius() 
+{
+  float invalid_temperature = -300;
+  float temperature         = -500;
+  byte  data[9], addr[8];
+  // data[] : Data read from the scratchpad
+  // addr[] : Address of 1-Wire detected sensor
+ 
+  // Look for the next 1-Wire sensor available
+  if (!ds.search(addr)) 
+  {
+    Serial.println("No sensor found.");
+    return invalid_temperature;
+  }
+  
+  // Check the received address
+  if (OneWire::crc8(addr, 7) != addr[7]) 
+  {
+    Serial.println("Invalid address.");
+    return invalid_temperature;
+  }
+ 
+  // Make sure that the sensor type is DS18B20
+  if (addr[0] != 0x28) 
+  {
+    Serial.println("Invalid sensor.");
+    return invalid_temperature;
+  }
+ 
+  // Reset 1-Wire bus and select the sensor
+  ds.reset();
+  ds.select(addr);
+  
+  // Take the measurement
+  ds.write(0x44, 1);
+  delay(800);
+  
+  // Reset 1-Wire bus and read the scratchpad
+  ds.reset();
+  ds.select(addr);
+  ds.write(0xBE);
+ 
+  for (byte i = 0; i < 9; i++) {
+    data[i] = ds.read();
+  }
+   
+  // Convert raw temperature into Celsius degree
+  temperature = (int16_t) ((data[1] << 8) | data[0]) * 0.0625; 
+  
+  return temperature;
 }
 
 void setup() 
@@ -45,37 +104,71 @@ void setup()
     Serial.println("Card failed, or not present");
     return;
   }
+  else 
+  {
+    Serial.println("Card initialized");
+    writeOnSDcard(columnNames);
+  }
+
+  // --- INITIALIZATION OF RTC ---
+  if (! rtc.begin()) 
+  {
+    Serial.println("Couldn't find RTC");
+    return;
+  }
+  else 
+  {
+  rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+  }
   
-  Serial.println("Card initialized");
-  writeOnSDcard(columnNames);
 
 }
 
 void loop() 
 {
-
-  // --- READ SINGLE BATTERY VOLTAGE ---
+  // --- BATTERY VOLTAGE ---
   int singleBattery_value = analogRead(SINGLE_BATTERY_PIN);
   
   // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
   float singleBattery_voltage = float(singleBattery_value) * (5.00 / 1023.00);
 
-  // Print out values
-  Serial.print("Battery : ");
-  Serial.print(singleBattery_voltage);
-  Serial.println(" V");
+  // --- TEMPERATURE ---
+  float temperature = getTemperatureInCelsius();
+
+
+
   
   // --- WRITE ON SD CARD ---
-  time = millis();
-  dataString = String("");
-  dataString += time;
-  dataString += ';';
+
+  // Write Time
+
+  // Write Battery Voltages
+
+  // Write Temperature
+  
+  DateTime now = rtc.now();
+  
+  dataString  = String("");
+  dataString += String(now.day());
+  dataString += "/";
+  dataString += String(now.month());
+  dataString += "/";
+  dataString += String(now.year());
+  dataString += " ";
+  dataString += String(now.hour());
+  dataString += ":";
+  dataString += String(now.minute());
+  dataString += ":";
+  dataString += String(now.second());
+  dataString += ";";
   dataString += singleBattery_voltage;
+  dataString += ";";
+  dataString += temperature;
+  
+
   writeOnSDcard(dataString);
+  Serial.println(dataString);
 
   delay(LOOP_TIME_MS); 
 
 }
-
-
-
